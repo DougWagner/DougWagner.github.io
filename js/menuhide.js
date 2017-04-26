75398af393505cd8d762df3780f14d6bc45637b9
DougWagner <digwagner@gmail.com>
2017-04-26 03:33:42 -0700

    Added my email address

--- js/menuhide.js
+++ js/menuhide.js
@@ -1,3 +1,11 @@
 $('.basic-nav-link').click(function(){
     $('.navbar-collapse').collapse('hide');
 });
+
+$('.navbar-brand').click(function(){
+    $('.navbar-collapse').collapse('hide');
+});
+
+$('.container').click(function(){
+    $('.navbar-collapse').collapse('hide');
+});
